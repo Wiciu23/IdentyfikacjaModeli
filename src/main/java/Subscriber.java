36629432583b2eafcbaa7ecdb1fbfc4efccf1743
Wiciu <pwitek@student.agh.edu.pl>
2023-05-02 12:19:03 +0200

    Set relative data path

--- src/main/java/Subscriber.java
+++ src/main/java/Subscriber.java
@@ -1,5 +1,5 @@
 public class Subscriber {
     public void subscribe(Publisher publisher){
-        publisher.getObservable().
+        publisher.getObservable();
     }
 }
--- src/main/java/Testy.java
+++ src/main/java/Testy.java
@@ -6,7 +6,7 @@ public class Testy {
         public static void main(String args[]) throws IOException {
             //double[] a = {1.0526579370455271E-5, 15.651291777393416, -2.6004906689578218, 0.0026674853755454184, -2.5946715736381925, -0.0422887220521262, 0.0, 0.452, -0.017180035236922072, 0.409, 0.0, 4.2E-5, 0.00621783922304361};
             double[] a = {1.1137460501497857E-4, 31049.141541228888, 94651.45815004251, 1.5951E9, 234211.3397316366, 2.769637106115258, 0.0, 0.452, 0.27810350762520614, 0.409, 0.0, 4.2E8, 0.07907011963088659};
-            ObjectProperties[] dataModel = ExcelReader.getObjectPropertiesExcel("C:\\Users\\java\\Particle_swarm\\IdentyfikacjaModeli\\Dane_lab5.xlsx");
+            ObjectProperties[] dataModel = ExcelReader.getObjectPropertiesExcel("Dane_lab5.xlsx");
             double Q = 312000;
             int j =0;
             for (ObjectProperties data :
